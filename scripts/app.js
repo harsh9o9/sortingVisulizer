136b45ac5af6d25d0c728e426550a165cd574373
harsh9o9 <hbhardwaj454@gmail.com>
2022-09-03 13:33:02 +0530

    code optimized

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,18 @@
 const start = async () => {
   let algoValue = Number(document.querySelector(".algo-menu").value);
   let speedValue = Number(document.querySelector(".speed-menu").value);
+  let arrValue = Number(document.querySelector(".size-menu").value);
 
   if (speedValue === 0) {
     speedValue = 1;
   }
   if (algoValue === 0) {
     alert("No Algorithm Selected");
+    return;
+  }
+
+  if (arrValue === 0) {
+    alert("No Size Selected");
     return;
   }
 
@@ -19,46 +25,8 @@ const start = async () => {
   if (algoValue === 5) await algorithm.QuickSort();
 };
 
-const RenderScreen = async () => {
-  let algoValue = Number(document.querySelector(".algo-menu").value);
-  await RenderList();
-};
-
-const RenderList = async () => {
-  let sizeValue = Number(document.querySelector(".size-menu").value);
-  await clearScreen();
-
-  let list = await randomList(sizeValue);
-  const arrayNode = document.querySelector(".array");
-  console.log(arrayNode);
-  console.log(list);
-  for (const element of list) {
-    const node = document.createElement("div");
-    node.className = "cell";
-    node.setAttribute("value", String(element));
-    node.style.height = `${3.8 * element}px`;
-    arrayNode.appendChild(node);
-  }
-};
-
-const RenderArray = async (sorted) => {
-  let sizeValue = Number(document.querySelector(".size-menu").value);
-  await clearScreen();
-
-  let list = await randomList(sizeValue);
-  if (sorted) list.sort((a, b) => a - b);
-
-  const arrayNode = document.querySelector(".array");
-  const divnode = document.createElement("div");
-  divnode.className = "s-array";
-
-  for (const element of list) {
-    const dnode = document.createElement("div");
-    dnode.className = "s-cell";
-    dnode.innerText = element;
-    divnode.appendChild(dnode);
-  }
-  arrayNode.appendChild(divnode);
+const clearScreen = async () => {
+  document.querySelector(".array").innerHTML = "";
 };
 
 const randomList = async (Length) => {
@@ -75,21 +43,22 @@ const randomList = async (Length) => {
   return list;
 };
 
-const clearScreen = async () => {
-  document.querySelector(".array").innerHTML = "";
-};
+const RenderList = async () => {
+  let sizeValue = Number(document.querySelector(".size-menu").value);
+  await clearScreen();
 
-const response = () => {
-  let Navbar = document.querySelector(".navbar");
-  if (Navbar.className === "navbar") {
-    Navbar.className += " responsive";
-  } else {
-    Navbar.className = "navbar";
+  let list = await randomList(sizeValue);
+  const arrayNode = document.querySelector(".array");
+  for (const element of list) {
+    const node = document.createElement("div");
+    node.className = "cell";
+    node.setAttribute("value", String(element));
+    node.style.height = `${3.8 * element}px`;
+    arrayNode.appendChild(node);
   }
 };
 
-document.querySelector(".icon").addEventListener("click", response);
 document.querySelector(".start").addEventListener("click", start);
-document.querySelector(".size-menu").addEventListener("change", RenderScreen);
-document.querySelector(".algo-menu").addEventListener("change", RenderScreen);
-window.onload = RenderScreen;
+document.querySelector(".size-menu").addEventListener("change", RenderList);
+document.querySelector(".algo-menu").addEventListener("change", RenderList);
+window.onload = RenderList;
